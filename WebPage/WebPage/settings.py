d0bb3c99272ea5eea2d2cfc118a71c755b64fd16
Will-Fs <will.fs.martian@gmail.com>
2022-03-07 10:24:51 -0500

    Updated for gitignore

--- WebPage/WebPage/settings.py
+++ WebPage/WebPage/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from SpotSite import secrets
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8dguftjlfig)(8expfe7+6=w*_(%_mxjx0!rs9x4c+v6&qhndw'
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
