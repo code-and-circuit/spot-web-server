db55577b4022af0f8b41b827a3df737755e5bf89
Will Scheirey <williamscheirey@gmail.com>
2023-10-23 14:43:58 -0400

    add shell scripts
    Only MacOS right now, but this will be updated soon

---
+++ Shell Scripts/MacOS/run.sh
@@ -0,0 +1,11 @@
+osascript -e 'tell app "Terminal"
+    do script "cd ~/Documents/Github/Scratch/scratch-vm && export NODE_OPTIONS=--openssl-legacy-provider && npm run watch"
+end tell'
+
+osascript -e 'tell app "Terminal"
+    do script "cd ~/Documents/Github/Scratch/scratch-gui && export NODE_OPTIONS=--openssl-legacy-provider && npm start"
+end tell'
+
+osascript -e 'tell app "Terminal"
+    do script "cd ~/Documents/Github/spot-web-server/WebPage && python3 -m uvicorn WebPage.asgi:application --host 0.0.0.0 --reload"
+end tell'
---
+++ Shell Scripts/MacOS/test.sh
@@ -0,0 +1,5 @@
+cd ~/Documents/GitHub && touch spot-web-server/WebPage/SpotSite/secrets.py
+echo "ROBOT_USERNAME = \"[YOUR SPOT USERNAME]\"
+ROBOT_PASSWORD = \"[YOUR SPOT PASSWORD]\"
+SECRET_KEY = \"dummy secret key\" # This does not have to be changed
+ROBOT_IP = \"[ROBOT_IP]\"" > spot-web-server/WebPage/SpotSite/secrets.py
