20bd757fe9f379ab73712d6baaa579b0358d0eb4
Will Scheirey <williamscheirey@gmail.com>
2023-10-24 10:05:20 -0400

    windows shell scripts (not working)

---
+++ Shell Scripts/Windows/install.sh
@@ -0,0 +1,29 @@
+# GitHub directory
+mkdir ~/Documents/GitHub && cd ~/Documents/GitHub
+
+# Web Server
+git clone https://github.com/code-and-circuit/spot-web-server.git
+
+echo "ROBOT_USERNAME = \"[YOUR SPOT USERNAME]\"
+ROBOT_PASSWORD = \"[YOUR SPOT PASSWORD]\"
+SECRET_KEY = \"dummy secret key\" # This does not have to be changed
+ROBOT_IP = \"[ROBOT_IP]\"" > spot-web-server/WebPage/SpotSite/secrets.py
+
+pip3 install -r spot-web-server/WebPage/requirements.txt
+
+# Clone Scratch
+git clone https://github.com/kaspesla/scratch-gui.git
+git clone https://github.com/kaspesla/scratch-vm.git
+
+# Node Stuff
+cd scratch-vm
+npm install --legacy-peer-deps
+npm link --legacy-peer-deps
+
+cd ../scratch-gui
+npm install --legacy-peer-deps
+npm link scratch-vm --legacy-peer-deps
+
+# Open files to write constants
+cd ..
+start notepad spot-web-server/WebPage/SpotSite/secrets.py
